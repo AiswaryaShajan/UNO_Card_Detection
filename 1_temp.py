72344e465d4307d6d411746aa19fef87da4e1d23
Aiswarya Shajan <AS4213@live.mdx.ac.uk>
2025-04-08 10:25:11 +0400

    '.'

--- 1_temp.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-import numpy as np
-
-img=cv.imread('templates/1_temp.jpg')
-gray_1_temp  = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('gray',gray_1_temp)
-cv.imwrite('processed_templates/1_temp.jpg', gray_1_temp)
-cv.waitKey(0)
-cv.destroyAllWindows
